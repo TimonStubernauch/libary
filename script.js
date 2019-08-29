b6c85f9cc66cfccef7950ade1c1d66a55763449b
Timon Stubenrauch <Timon.Stubenrauch@gmail.com>
2019-08-29 09:10:05 +0200

    Integrade Firebase

--- script.js
+++ script.js
@@ -2,24 +2,50 @@ window.onload=setup;
 let lib =[];
 //{title:'Age of Myth', author:'Michael J. Sullivan', read:false, index:0},{title:'Theft of Swords', author:'Michael J. Sullivan', read:true, index:1}
 const bookContainer = document.querySelector('#bookContainer')
+
+const database = firebase.database();
+const ref = database.ref('books');
+
+
 function setup() {
-    let b1 = new Book('b1','a1',true);
-    b1.add()
-    let b2 = new Book('b2','a2',true);
-    b2.add()
+    ref.once('value').then((snapshot)=>{
+        if (snapshot.exists()) ref.on('value',gotBooks,errData);        
+    });  
+    render();  
 }
+
+function gotBooks (data){
+    lib=[];
+    var books = data.val();
+    if (books!=null){
+        var keys = Object.keys(books);
+
+        for(let i = 0; i<keys.length; i++){
+            let k =keys[i]; 
+            let newBook = new Book(books[k].title, books[k].author, books[k].read);
+            newBook.setDbKey(k);      
+            lib.push(newBook);
+        }
+        render();
+    }
+    
+}
+
+function errData(err){
+    console.log(err);
+}
+
 function render (){
     delDisplay();
-    
     lib.forEach(book => {
-        book.index=lib.indexOf(book);
 
+        book.index=lib.indexOf(book);
         let display = document.createElement('div');
         display.setAttribute('bookIndex',book.index);
-        display.classList.add('bookDisplay')
+        display.classList.add('bookDisplay');
     
         let title = document.createElement('h2');
-        title.textContent = book.title
+        title.textContent = book.title;
     
         let author = document.createElement('h3');
         author.textContent = 'by ' + book.author;
@@ -29,9 +55,11 @@ function render (){
         if(book.read)read.checked=true;
         let index = book.index;
         read.addEventListener('change',function(e){
-            let bookIndex =  e.target.parentElement.getAttribute('bookIndex')
+            let bookIndex =  e.target.parentElement.parentElement.getAttribute('bookindex');
+            console.log(e.target.parentElement.parentElement.getAttribute('bookIndex'));
+            
             lib[bookIndex].read =!lib[bookIndex].read;
-            console.log(lib);       
+            ref.child(book.dbKey).update(book);
             
         });
         read.classList.add('checkbox');
@@ -58,6 +86,7 @@ function render (){
         bookContainer.appendChild(display);       
     
     });
+
 }
 
 function removeBook (bookIndex){
@@ -66,6 +95,7 @@ function removeBook (bookIndex){
 };
 
 function delDisplay() {
+
     do {
         let toBeDeleted = bookContainer.childNodes;
         toBeDeleted.forEach(element => {
@@ -80,20 +110,16 @@ function Book (title,author,read){
     this.author=author,
     this.read=read,
     this.index=lib.length;
+    this.dbKey='';
 }
-Book.prototype.add = function (){
-    lib.push(this);
-    render();
 
-    
-};
+Book.prototype.setDbKey = function(key){
+    this.dbKey=key;
+}
 
 Book.prototype.del = function (){
-    console.log(lib);
     lib.splice(this.index,1);
-    console.log(lib);
-    console.log('del '+this.index);
-    
+    ref.child(this.dbKey).remove();    
     render();
 };
 const addBookMenu = document.querySelector('#addBookMenu');
@@ -124,11 +150,32 @@ function addBook() {
     let title = addBookTitle.value; 
     let author = addBookAuthor.value;
     let read = addBookRead.checked;
-    lib.push(new Book(title,author,read));
+    let newBook = new Book(title,author,read) ;
+    lib.push(newBook);
     
+    addBookToDatabase(newBook);
     addBookTitle.value='';
     addBookAuthor.value='';
     addBookRead.checked = false;
 
     render();    
 }
+
+function addBookToDatabase (newBook){
+    
+    let data ={
+        title: newBook.title, 
+        author:newBook.author,
+        read:newBook.read,
+        index: newBook.index
+    }
+    ref.push(data);
+}
+// window.onunload=saveData;
+// const database = firebase.database();
+//     
+// function saveData(){
+    
+//     console.log('unload')
+// }
+
